8593b5eeb9f38d0ff96653ac9815720d315816f2
Abhiram <74865607+abhi98ram@users.noreply.github.com>
2020-11-28 11:49:07 +0530

    docs: added more questions

--- Game.js
+++ Game.js
@@ -1,56 +1,64 @@
 var readlineSync = require("readline-sync");
 
-const chalk = require('chalk');
+var score=0;
 
-var score = 0;
-var userName = readlineSync.question("what is your name  ");
+var chalk = require("chalk");
+
+var username = readlineSync.question("What is your name  ");
 console.log("\n");
-console.log("welcome " + userName  + "  Learn Math with FUN !!!!");
+console.log(chalk.green("Welcome " + username  + " To Learn Math with Fun !!!!"));
 console.log("\n");
+function play(question,option,answer) {
+  console.log(chalk.bold(question));
+  console.log("\n");
+  console.log(option);
+  console.log("\n");
+  var currentanswer = readlineSync.question(chalk.cyan.bold("Choose the correct Option : "));
+  console.log('\n');
+  if(currentanswer === answer) {
+    console.log(chalk.green("RIGHT ANSWER"));
+    
+    score = score+1;
+    
+    console.log("----------------------------------------------------------------");
+    console.log("\n");
+  }else {
+    console.log(chalk.red("WRONG"));
+    console.log("\n");
+    console.log("The Right Answer is " + chalk.green(answer));
 
-function play(question,question2,question3,answer) {
-   console.log(question);
-   console.log("\n");
-   console.log(question2);
-   console.log("\n");
-   var currentanswer=readlineSync.question(question3);
-   if(currentanswer === answer) {
-     console.log(chalk.green("A.10")+" B)20"+" c)30"); 
-     
-     score = score+1;
-   }else {
-     console.log(chalk.green("A.10")+" B)20"+" c)30");
-     
+    console.log("-----------------------------------------------------------------");
 
-   }
-   
-   console.log(score);
-   console.log("-------");
+    
+    
+    console.log("\n");
+  }
+
+  
+
 }
 
 var questions = [
   {
-    question:"1. 10% of 100 ",
-    question2:"A) 10   B) 20   C) 30",
-    question3:"Choose your option  ",
+    question:"1.What is the probability that divisor of 1099 is a multiple of 1096?",
+    option:"A) 1/625 B) 4/625 C) 12/625 D) 16/625",
     answer:"A",
   },
   {
-    question:"2. 100 is what percent of 1000",
-    question2:"A) 10   B) 20    C) 30",
-    question3:"What is your option  ",
-    answer:"A",
+    question:"2.(1015)2 = ?",
+    option:"A) 1041025 B) 1030225 C) 1050125 D) 1025125",
+    answer:"B",
+  },
+  {
+    question:"3.Which one is NOT divisible by 3 ?",
+    option:"A) 5271 B) 6291 C) 4372 D) 8952",
+    answer:"C",
   }
 ];
 
-for(var i=0;i<=questions.length-1;i=i+1) {
-  var currentquestion = questions[i];
-  play(currentquestion.question,currentquestion.question2,currentquestion.question3,currentquestion.answer);
-}
-console.log("\n");
-if(score ===0){
-  console.log("your score is " + chalk.red(score));
-}else {
-  console.log("your score is " + chalk.green(score));
+for(var i=0;i<questions.length;i=i+1) {
+  currentquestion=questions[i];
+  play(currentquestion.question,currentquestion.option,currentquestion.answer);
 }
 
+console.log(chalk.bold("Your Total score is ") + chalk.green.bold(score));
